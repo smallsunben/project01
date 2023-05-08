e1ae0d88e60532ec06b4f36fdc3f72f521d329e7
lele <1611804889@qq.com>
2023-05-08 15:05:21 +0800

    修改index相关文件

---
+++ index.js
@@ -0,0 +1,27 @@
+let i = 1;
+function mouseMove() {
+    document.querySelector('.box').innerHTML = i++
+}
+// function throttle(fn, t) {
+//     let timer = null;
+//     return () => {
+//         if (!timer) {
+//             timer = setTimeout(function () {
+//                 fn()
+//                 timer = null;
+//             }, t)
+//         }
+//     }
+// }
+// document.querySelector('.box').addEventListener('mousemove', throttle(mouseMove, 1000))
+
+function debounce(fn, t) {
+    let timer = null;
+    return () => {
+        if (timer) clearTimeout(timer);
+        timer = setTimeout(function () {
+            fn()
+        }, t)
+    }
+}
+document.querySelector('.box').addEventListener('mousemove', debounce(mouseMove, 1000))
